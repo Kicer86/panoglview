7380e63c4208b077218022f0fac59a12b9b4ab45
Bruno Postle <bruno@postle.net>
2019-01-11 17:45:43 +0000

    support both wx2 and wx3 (Iouri Ivliev via FreeBSD)

--- src/panointeractivecanvas.h
+++ src/panointeractivecanvas.h
@@ -39,7 +39,11 @@ public:
   void OnMouse(wxMouseEvent& event);
   void OnTimer(wxTimerEvent &event);
   void OnPaint(wxPaintEvent &event);
+#if wxCHECK_VERSION(3,0,0)
+  void resized(wxSizeEvent &event);
+#else //wxCHECK_VERSION(3,0,0)
   void OnSize(wxSizeEvent &event);
+#endif //wxCHECK_VERSION(3,0,0)
   void setBoundaryMode(int boundarymode);
   void enableShowBoundaries(bool show);
   void enableUseBoundaries(bool use);
